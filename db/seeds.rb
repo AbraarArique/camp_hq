1c3b6733be10ca73f12980531738aeed8363c73a
Abraar Arique <abraar@abraararique.com>
2015-12-14 23:05:42 +0600

    Update seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,34 @@
 User.create(
-  name: 'N1',
-  email: 'E1',
-  about: 'A1',
-  status: 'S1'
+  name: 'John Stone',
+  email: 'john@camphq.com',
+  about: 'John is a skilled Ruby programmer who has been writing code for 5 years. He loves to code and program software.',
+  status: 'Available'
 )
 User.create(
-  name: 'N2',
-  email: 'E2',
-  about: 'A2',
-  status: 'S2'
+  name: 'Jenny Hills',
+  email: 'jenny@camphq.com',
+  about: 'Jenny is the Ruby on Rails expert of our team. She handles all the back-end of our RoR apps.',
+  status: 'Available/Online'
 )
 User.create(
-  name: 'N3',
-  email: 'E3',
-  about: 'A3',
-  status: 'S3'
+  name: 'Steve Gems',
+  email: 'steve@camphq.com',
+  about: 'Steve is a highly experiecned AWS consultant. He helps us deploy and setup Amazon EC2 and S3 instances.',
+  status: 'Working/Online'
 )
 
 Task.create(
-  name: 'Name1',
-  description: 'Description1',
-  completed: 1
+  name: 'EC2 Setup',
+  description: 'Initialize an Amazon EC2 t2.micro instance for RoR prototype.',
+  completed: 0
 )
 Task.create(
-  name: 'Name2',
-  description: 'Description2',
-  completed: 1
+  name: 'Fix 500 Error',
+  description: 'Project #98A5 is experiencing a 500 "Something went wrong" error.',
+  completed: 0
 )
 Task.create(
-  name: 'Name3',
-  description: 'Description3',
+  name: 'Create RoR Prototype',
+  description: 'Create the first prototype of project #98A5.',
   completed: 1
 )
